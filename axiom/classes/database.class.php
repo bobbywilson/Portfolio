c05de67b6fe5fc19c8a7b702bfceedb3cf594be4
bobbywilson <email.bobby.wilson@gmail.com>
2015-05-09 16:08:05 -0400

    Security Update
    Added database class back without password.

---
+++ axiom/classes/database.class.php
@@ -0,0 +1,90 @@
+
+
+<?php  
+
+	// Database Class
+
+	class Database {
+	
+	
+		private $host 	  = "localhost";
+		private $database = "axiom";
+		private $user     = "****";
+		private $password = "****";
+		
+		public $conn="";
+	
+	
+	/* constructor */
+		
+		public function __construct() {
+		
+			$this->db_connect();
+		
+		}
+	
+	/* destructor */
+	
+		public function __destruct() {
+		
+			$this->conn = null;
+		
+		}
+		
+	/* get function */
+		
+		public function __get($name) {
+		
+			return $this->$name;
+		
+		}
+		
+	/* set function */
+		
+		public function __set($name, $value) {
+			
+			$this->$name=$value;
+		
+		}
+		
+		
+	/* Database Connection Method */
+		
+		
+		private function db_connect() {
+		
+			// Connect to Database
+		
+			try {
+				
+				$conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->database, $this->user, $this->password);
+				
+				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				
+				$conn->exec("SET NAMES 'utf8'");
+				
+					
+				
+			}
+			
+			// Catch Errors
+			
+			catch (PDOException $error) {
+				
+				echo "Unable to connect to database: " . $error->getMessage();
+					
+				exit();
+					
+			}
+			
+				$this->conn = $conn;		
+			
+		}
+
+
+		
+	}
+
+
+
+?>
