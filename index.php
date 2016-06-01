b3fa4538e41fe255ccf9b3bb6b3a7177b793694b
Bobby Wilson <email.bobby.wilson@gmail.com>
2016-06-01 12:54:39 -0400

    update

--- index.php
+++ index.php
@@ -19,14 +19,19 @@
 		</br>-->
 		
 		<h2 class="text-center">About Me</h2></br>
-		<p class="body-font">Hello, my name is Bobby Wilson and I am a Web Developer. I am a Object Oriented Programmer (OOP). I primarily build web systems using HTML5, CSS3, JavaScript, PHP, MySQL and Bootstrap. I am skilled at building mobile and responsive layouts. I also have experience with GitHub for version control, backup repository and collaboration.
+		<p class="body-font">Hello, my name is Bobby Wilson and I am a Web Developer. 
+			I am a Object Oriented Programmer (OOP). I primarily build web systems using HTML5, CSS3, 
+			JavaScript, PHP, MySQL and Bootstrap. I am skilled at building mobile and responsive layouts. 
+			I also have experience with GitHub for version control, backup repository and collaboration.
 		</p>
 		
-		<p class="body-font"> I am currently working as a Microsoft.Net Developer Apprentice at Eleven Fifty Consulting. In this role, I am learning C# and the Microsoft.Net framework in order to support the consulting team.
+		<p class="body-font"> I am currently working as a Microsoft.Net Developer Apprentice at Eleven
+			Fifty Consulting. In this role, I am learning C# and the Microsoft.Net framework in order to support the consulting team.
 		</p>
 		
 		<p class="body-font"> Below is an sample of my work. You can see all of the projects that I have worked on by clicking the 
-		<a style="color: #333 !important; text-decoration: underline !important; font-weight: bold;" href="<?php echo ABSOLUTE_PATH . "portfolio/"?>">My Portfolio</a> link.
+		<a style="color: #333 !important; text-decoration: underline !important; font-weight: bold;" href="<?php echo ABSOLUTE_PATH . 
+		"portfolio/"?>">My Portfolio</a> link.
 		</p>
 		
 		</br></br>
